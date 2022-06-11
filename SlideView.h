fb96603e5dfcc8eee75c4d58f66ce1a238fd2f45
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-06-11 16:25:41 -0300

    fix: :bug: fixing map render function

--- SlideView.h
+++ SlideView.h
@@ -13,56 +13,59 @@
 #include <iostream>
 using namespace std;
 
-class SlideView : public TilemapView {
+class SlideView : public TilemapView
+{
 public:
-    void computeDrawPosition(const int col, const int row, const float tw, const float th, float &targetx, float &targety) const {
-        targetx = col * tw + row * tw/2;
-        targety = row * th / 2;
+    void computeDrawPosition(const int col, const int row, const float tw, const float th, float &targetx, float &targety) const
+    {
+        targetx = col * tw / 2 + row * tw / 2;
+        targety = col * th / 2 - row * th / 2;
     }
-    
-    void computeMouseMap(int &col, int &row, const float tw, const float th, const float mx, const float my) const {
+
+    void computeMouseMap(int &col, int &row, const float tw, const float th, const float mx, const float my) const
+    {
         float tw2 = tw / 2.0f;
         float th2 = th / 2.0f;
-        
+
         row = my / th2;
         col = (mx - row * tw2) / tw;
         // cout << "dest: " << col << "," << row << endl;
-
     }
-    
-    void computeTileWalking(int &col, int &row, const int direction) const {
-        switch(direction){
-            case DIRECTION_NORTH: 
-                col--; 
-                row += 2;
-                break;
-            case DIRECTION_EAST: 
-                col++;
-                break;
-            case DIRECTION_SOUTH: 
-                col++; 
-                row -= 2;
-                break;
-            case DIRECTION_WEST: 
-                col--;
-                break;
-            case DIRECTION_NORTHEAST: 
-                row++;
-                break;
-            case DIRECTION_SOUTHEAST: 
-                col++; 
-                row--;
-                break;
-            case DIRECTION_SOUTHWEST: 
-                row--;
-                break;
-            case DIRECTION_NORTHWEST: 
-                col--; 
-                row++;
-                break;       
+
+    void computeTileWalking(int &col, int &row, const int direction) const
+    {
+        switch (direction)
+        {
+        case DIRECTION_NORTH:
+            col--;
+            row += 2;
+            break;
+        case DIRECTION_EAST:
+            col++;
+            break;
+        case DIRECTION_SOUTH:
+            col++;
+            row -= 2;
+            break;
+        case DIRECTION_WEST:
+            col--;
+            break;
+        case DIRECTION_NORTHEAST:
+            row++;
+            break;
+        case DIRECTION_SOUTHEAST:
+            col++;
+            row--;
+            break;
+        case DIRECTION_SOUTHWEST:
+            row--;
+            break;
+        case DIRECTION_NORTHWEST:
+            col--;
+            row++;
+            break;
         }
-    } 
-    
+    }
 };
-    
+
 #endif /* SlideView_h */
